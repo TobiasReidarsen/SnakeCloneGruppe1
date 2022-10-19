3fc78f3adbf2d94fb708cf3a35c83f9baa6652c1
WinstonSmith314 <tobias.reidarsen@gmail.com>
2022-10-19 11:17:03 +0200

    Made a draw board function

---
+++ DrawBoard.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace SnakeCloneGruppe1
+{
+    public class DrawBoard
+    {
+        public static void DrawBoard_()
+        {
+            int height = 7;
+            int width = 7;
+            char wall = '#';
+            int columnCount = height * width;
+            List<string> board = new List<string>();
+
+            // wall = #
+
+            for (int i = 0; i < columnCount; i++)  // i står for width
+            {
+                for (int j = 0; j < columnCount; j++)
+                {
+                    if (i == 0 || i == columnCount - 1)
+                    {
+                        Write(wall);
+                    } 
+                    else if (j == 0 || j == columnCount - 1)
+                    {
+                        Write(wall);
+                    }
+                    else
+                    {
+                        Write(' ');
+                    }
+                }
+                WriteLine();
+            }
+        }
+    }  
+}
+
--- Program.cs
+++ Program.cs
@@ -1,10 +1,13 @@
-﻿namespace SnakeCloneGruppe1
+﻿using static System.Console;
+namespace SnakeCloneGruppe1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            WriteLine("Welcome to Snake.\nA totally original game by team1!");
+            DrawBoard.DrawBoard_();
+            Console.ReadKey();
         }
     }
 }
